bfd664c3a50cd1eeba2737c56824068a9c9b671a
AmauriBlanco <blancoamauri@gmail.com>
2022-03-31 15:07:18 -0300

    1-5 Criando header

--- includes/header.php
+++ includes/header.php
@@ -8,9 +8,23 @@
     <!-- CSS -->
     <link rel="stylesheet" href="src/style.css">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
     <title>STORE</title>
 </head>
 <body>
-
+    <header>
+        <div class="container">
+            <div class="row justify-content-center justify-content-md-between align-items-center gap-3 gap-md-0 py-4">
+                <a href="/home.php" class="col-6"><img src="img/logo-treinaweb.webp" alt="Logo da TreinaWeb" width="200px"></a>
+                <div class="col-12 col-md-4 col-xl-3">
+                    <form class="search-area rounded-3 p-3 d-flex flex-nowrap">
+                        <i class="bi bi-search me-2"></i>
+                        <input type="text" placeholder="Pesquisar produtos...">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
     
 
