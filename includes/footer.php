2acaf9deea49ddadd2bcad3ab5e4c78fea2b0a7d
AmauriBlanco <blancoamauri@gmail.com>
2022-04-05 17:28:36 -0300

    1-9 Iniciando com o Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,58 @@
 
+    <footer class="container-fluid footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg">
+                    <a href="/home.php"><img src="img/logo-treinaweb.webp" alt="Logo da TreinaWeb" width="200px"></a>
+                    <p class="mt-3">
+                        Avepark - Parque da Ciência e<br>
+                        Tecnologia, S.A.<br>
+                        4805-017 Guimarães
+                    </p>
+                    <div>
+                        <i class="bi bi-facebook fs-3 me-3"></i>
+                        <i class="bi bi-instagram fs-3"></i>
+                    </div>
+                </div>
+                <div class="col-lg offset-lg-2 d-flex flex-column">
+                    <h4>Website</h4>
+                    <a href="#">Home</a>
+                    <a href="#">Sobre Nós</a>
+                    <a href="#">Loja</a>
+                    <a href="#">Contato</a>
+                    <a href="#">A minha conta</a>
+                </div>
+                <div class="col-lg d-flex flex-column">
+                    <h4>Website</h4>
+                    <a href="#">Home</a>
+                    <a href="#">Sobre Nós</a>
+                    <a href="#">Loja</a>
+                    <a href="#">Contato</a>
+                    <a href="#">A minha conta</a>
+                </div>
+                <div class="col-lg d-flex flex-column">
+                    <h4>Website</h4>
+                    <a href="#">Home</a>
+                    <a href="#">Sobre Nós</a>
+                    <a href="#">Loja</a>
+                    <a href="#">Contato</a>
+                    <a href="#">A minha conta</a>
+                </div>
+                <div class="col-lg d-flex flex-column">
+                    <h4>Website</h4>
+                    <a href="#">Home</a>
+                    <a href="#">Sobre Nós</a>
+                    <a href="#">Loja</a>
+                    <a href="#">Contato</a>
+                    <a href="#">A minha conta</a>
+                </div>
+            </div>
+            <div>
+                <hr>
+                <h5>© 2021, Inflight IT</h5>
+            </div>
+        </div>
+    </footer>
 
 
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
