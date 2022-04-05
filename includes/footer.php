744a0dfaa791f92938f65388621ada2955029263
AmauriBlanco <blancoamauri@gmail.com>
2022-04-05 15:24:35 -0300

    1-8 Finalizando Header

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 
 
 
-
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
     </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -24,6 +24,31 @@
                     </form>
                 </div>
             </div>
+            <nav class="navbar navbar-expand-md navbar-light">
+                <div class="container px-0">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link px-0" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link px-0 mx-md-3" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link px-0" href="#">Pricing</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <i class="bi bi-currency-dollar"> 0,00</i>
+                        <p class="mb-0 mx-3">0 itens</p>
+                        <a href="#"><img src="img/carrinho-icon.svg" alt="Icone de carrinho de compras"></a>
+                    </div>
+                </div>
+            </nav>
         </div>
     </header>
     
