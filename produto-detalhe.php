cfa4f99c052b6d252868478c3721ea7bd5314a0d
AmauriBlanco <blancoamauri@gmail.com>
2022-04-13 23:35:50 -0300

    1-29 Finalizando e adaptando para mobile

--- produto-detalhe.php
+++ produto-detalhe.php
@@ -12,6 +12,62 @@
         </div>
 
 
+        <!-- Detalhe Porduto -->
+        <section>
+            <div class="container">
+                <div class="row gap-4">
+                    <div class="col-12 col-md-5 bloco-carousel">
+                        <div id="carouselExampleIndicators" class="carousel-detalhe carousel slide" data-bs-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"><img src="/img/img-iphone.png" alt="" class="d-block w-100"></li>                    
+                                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"><img src="/img/img-iphone-2.png" alt="" class="d-block w-100"></li>
+                                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"><img src="/img/img-iphone-3.png" alt="" class="d-block w-100"></li>
+                                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"><img src="/img/img-iphone-4.png" alt="" class="d-block w-100"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="/img/img-iphone.png" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/img/img-iphone-2.png" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/img/img-iphone-3.png" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/img/img-iphone-4.png" class="d-block w-100" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6 detalhe-iphone">
+                        <div>
+                            <h2>Apple iphone 12 64gb</h2>
+                            <i class="bi bi-currency-dollar me-2"> 1.200</i>
+                        </div>
+                        <form>
+                            <label for="colorSelect text-muted mb-3">Cor</label>
+                            <select id="colorSelect" class="form-select mb-4">
+                                <option selected>Preto</option>
+                                <option value="1">Azul</option>
+                                <option value="2">Dourado</option>
+                                <option value="3">Branco</option>
+                            </select>
+                            <div class="d-flex">
+                                <input type="number" placeholder="1" class="me-3">
+                                <a href="#" class="btn-card">Adicionar</a>
+                            </div>
+                        </form>
+                        <div>
+                            <p class="text-muted">Ref: N/D</p>
+                            <p class="text-muted">Categoria: <a href="loja.php">Apple</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </main>
 
 
